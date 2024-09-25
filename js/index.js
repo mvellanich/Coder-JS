d2def32a4d908a97b38c533fc0f228b5af6c7f94
mvellanich <mvellanich1987@gmail.com>
2024-09-25 08:50:04 -0300

    Pruebas nuevas

--- js/index.js
+++ js/index.js
@@ -3,9 +3,10 @@ let visitantes = [];
 function registrarVisita(name, email, oficina) {
   visitantes.push({ name, email, oficina });
   // para guardar en localStorage
-  localStorage.setItem("productos", JSON.stringify(visitantes));
+  localStorage.setItem("visitas", JSON.stringify(visitantes));
 
   actualizarLista();
+  console.log(visitantes);
 }
 
 const formulario = document.getElementById("formulario");
@@ -16,101 +17,49 @@ register.addEventListener("click", (e) => {
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const oficina = document.getElementById("oficina").value;
+  const datosVisitante = [name, email, oficina];
   registrarVisita(name, email, oficina);
   formulario.reset();
+  Swal.fire({
+    icon: "success",
+    text: "Registro Exitoso",
+    iconColor: "#2B5F4D",
+    confirmButtonColor: "#2B5F4D",
+  });
 });
 
 function actualizarLista() {
   const listaVisita = document.getElementById("registrados");
   registrados.innerHTML = "";
   visitantes.forEach((p) => {
-    const li = document.createElement("li");
-    li.textContent =
-      " Nombre y Apellido: " +
-      p.name +
-      " Email: " +
-      p.email +
-      " Oficina a visitar: " +
-      p.oficina;
-    registrados.appendChild(li);
+    const ul = document.createElement("div");
+    ul.innerHTML = `
+      <b>Nombre y Apellido: </b>${p.name}
+      <b>Email: </b> ${p.email}
+      <b>Oficina: </b> ${p.oficina}
+      `;
+    registrados.appendChild(ul);
   });
 }
 
-/* let nameVisita;
-let ageVisita;
-let numRrhh = 0;
-let numComp = 0;
-let numTeso = 0;
-let numSec = 0;
-let startIssue = false;
+/* // Agregar el producto al localStorage
+let carrito = JSON.parse(localStorage.getItem("formulario")) || [];
+carrito.push({ name, email, oficina });
+localStorage.setItem("formulario", JSON.stringify(carrito)); */
 
-const issueButton = document.querySelector("#inicioTramite");
+// Función para guardar la reserva en localStorage
+// function guardarVisitante(visitantes) {
+//   let datosVisitantes =
+//     JSON.parse(localStorage.getItem("datosVisitante")) || [];
+//   datosVisitantes.push(visitantes);
+//   localStorage.setItem("datosVisitantes", JSON.stringify(datosVisitantes));
+// }
 
-issueButton.addEventListener("click", handleStartIssue);
+// function cargarVisitantes() {
+//   let datosVisitantes =
+//     JSON.parse(localStorage.getItem("datosVisitantes")) || [];
+//   datosVisitantes.forEach((visitantes) => actualizarLista(visitantes));
+// }
 
-function handleStartIssue() {
-  alert("Bienvenido al sistema de ingreso de nuestra institución");
-
-  alert(
-    "Elija su trámite: \n 1) Dejar una nota \n 2) Ir a una oficina \n 3) Salir"
-  );
-
-  let menu = prompt("Ingrese el numero correspondiente a la opción deseada");
-
-  switch (menu) {
-    case "1":
-      handleOption1();
-      break;
-    case "2":
-      handleOption2();
-      break;
-    default:
-      alert("Muchas gracias");
-  }
-
-  function handleOption1() {
-    nameVisita = prompt("Ingrese su nombre y apellido");
-    ageVisita = prompt("Ingrese su edad");
-    alert(
-      "Muchas gracias, " +
-        nameVisita +
-        "! Puede dejar la nota en Mesa de Entradas, planta baja. Hasta luego!"
-    );
-  }
-  function handleOption2() {
-    nameVisita = prompt("Ingrese su nombre y apellido");
-    ageVisita = prompt("Ingrese su edad");
-    alert(
-      "Elija una oficina: \n 1) RRHH \n 2) Compras \n 3) Tesoreria \n 4) Secretaría "
-    );
-    let numOficina = prompt(
-      "Ingrese el número correspondiente a la opción deseada"
-    );
-    if (numOficina == 1) {
-      numRrhh++;
-      alert(
-        "La oficina de Recursos Humanos se encuentra en el 1er Piso, será llamado con numero " +
-          numRrhh
-      );
-    } else if (numOficina == 2) {
-      numComp++;
-      alert(
-        "La oficina de Compras se encuentra en el 2do Piso, será llamado con numero " +
-          numComp
-      );
-    } else if (numOficina == 3) {
-      numTeso++;
-      alert(
-        "La oficina de Tesoreria se encuentra en el 3er Piso, será llamado con numero " +
-          numTeso
-      );
-    } else if (numOficina == 2) {
-      numSec++;
-      alert(
-        "La oficina de Secretaría se encuentra en el 1er Piso, será llamado con numero " +
-          numSec
-      );
-    }
-  }
-}
- */
+// guardarVisitante(visitantes);
+// cargarVisitantes();
